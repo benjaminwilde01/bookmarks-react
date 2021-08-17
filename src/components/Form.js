57942a770a5059a2b931ac5df93d1275916ffff3
Benjamin Wilde <bendavidwilde95@gmail.com>
2021-08-17 12:54:36 -0500

    working on edit functionality

--- src/components/Form.js
+++ src/components/Form.js
@@ -22,13 +22,11 @@ function Form() {
     }
 
     return (
-        <form onSubmit={ addBookmark }>
-
-                
-                <label htmlFor="title">Title:</label>
+        <form onSubmit={ addBookmark }>  
+                <label htmlFor="title">Name:</label>
                 <input onChange={(ev) => setTitle(ev.target.value) } type="text" id="title" name="title"  value={ title } required/>
 
-                <label htmlFor="url"></label>
+                <label htmlFor="url">URL:</label>
                 <input onChange={(ev) => setUrl(ev.target.value) } type="text" id="url" name="url"  value={ url } required/>
 
                 <button type="submit">Submit</button>
--- src/components/Show.js
+++ src/components/Show.js
@@ -2,32 +2,54 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
 const baseURL = 'http://localhost:3003/'
+let showForm = false
 
 const Show = () => {
+    
+    const [bookmarks, setBookmarks] = useState([])
 
-  const [bookmarks, setBookmarks] = useState([])
+    
+    
+    const toggleForm = () => {
+        showForm = !showForm
+        console.log(showForm)
+    }
+    
 
-  
-  useEffect(() => {
-      async function fetchData() {
-          const res = await axios.get(baseURL + 'bookmarks')
-          setBookmarks(res.data)
-      }
-      fetchData()
-  })
+    useEffect(() => {
+        async function fetchData() {
+            const res = await axios.get(baseURL + 'bookmarks')
+            setBookmarks(res.data)
+        }
+        fetchData()
+    })
 
-  function deleteBookmark (id) {
-      console.log(id)
-      const newBookmark = bookmarks.filter((bookmark) => bookmark.id !== id)
+    function deleteBookmark (id) {
+        console.log(id)
+        const newBookmark = bookmarks.filter((bookmark) => bookmark.id !== id)
 
-      setBookmarks(newBookmark)
-      
-      try {
-            axios.delete(`${baseURL}bookmarks/${id}`)
-      } catch (error) {
-          console.log(error)
-      }
-  }
+        setBookmarks(newBookmark)
+        
+        try {
+                axios.delete(`${baseURL}bookmarks/${id}`)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    
+    function updateBookmark (id) {
+        console.log(id)
+        const newBookmark = bookmarks.filter((bookmark) => bookmark.id !== id)
+
+        setBookmarks(newBookmark)
+        
+        try {
+            axios.patch(`${baseURL}bookmarks/${id}`)
+        } catch (error) {
+            console.log(error)
+        }
+    }
 
 
 
@@ -36,8 +58,16 @@ const Show = () => {
             {bookmarks.map((bookmark) => (
                 <div key={bookmark._id}>
                     <h3>{bookmark.title}</h3>
-                    <h3><a href={'http://' + bookmark.url} target='_blank'>{bookmark.url} </a></h3>
-                    <button onClick={() => deleteBookmark(bookmark._id)}  />
+                    <h3><a href={'http://' + bookmark.url} target='_blank' rel="noreferrer">{bookmark.url} </a></h3>
+                    <button onClick={() => deleteBookmark(bookmark._id)}>DELETE</button>  
+                    <button onClick={toggleForm}>EDIT</button> 
+                    { showForm &&
+                        <form onSubmit={() => updateBookmark(bookmark._id)}>
+                            <input type="text" name="title" id="title" />
+                            <input type="text" name="url" id="url" />
+                            <input  type="submit" value="Submit"/>
+                        </form>
+                    }
                 </div>
             ))}
         </div>
